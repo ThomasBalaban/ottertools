778f38cb0dc393688de527f9126232166288032e
Tom <balabaninteractive@gmail.com>
2024-09-22 23:50:23 -0400

    2

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/my-angular-app'));
+app.use(express.static(__dirname + '/dist/ottertools'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/my-angular-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ottertools/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
